98b3429b7187fab22ea5aa6e9ace69263a2240ea
SalmanStarneo <salman.alsamiri@gmail.com>
2018-08-19 21:12:32 +0100

    To do list
    first to do list

---
+++ DOM.js
@@ -0,0 +1,55 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+// var li = document.getElementsByTagName("li");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	// li.onclick=underline;
+	input.value = "";
+
+ var button =document.createElement("button");
+
+ button.appendChild(document.createTextNode("Delete"))
+
+ li.appendChild(button);
+
+ button.onclick=remove;
+}
+
+// function underline(event) {
+// 	event.target.parentNode.classList.toggle("done");
+// }
+
+
+ul.onclick = function(event){
+	var target = event.target;
+	target.classList.toggle("done");
+}
+
+function remove(event) {
+	event.target.parentNode.remove();
+}
+
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
